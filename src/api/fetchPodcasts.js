ff0b583900af3aa6f94d85028996b2fcbcf63472
sijabulile-ncube <s.mabhena@gmail.com>
2026-07-05 13:36:53 +0200

    modified:   src/api/fetchPodcasts.js

--- src/api/fetchPodcasts.js
+++ src/api/fetchPodcasts.js
@@ -1,11 +1,11 @@
 /**
  * @function fetchPodcasts
- * Asynchronously fetches podcast data from the remote API and updates state accordingly.
- * Handles loading, error, and successful data response via provided state setters.
+ * Podcast date fetched from the remote API and updates state accordingly.
+ * Errors handled, loading error, and successful data response via provided state setters.
  *
- * @param {Function} setPodcasts - State setter function to update the podcasts array.
- * @param {Function} setError - State setter function to update the error message (string).
- * @param {Function} setLoading - State setter function to toggle the loading state (boolean).
+ * @param {Function} setPodcasts - function to update the podcasts array.
+ * @param {Function} setError - function to update the error message (string).
+ * @param {Function} setLoading - function to toggle the loading state (boolean).
  *
  * @returns {Promise<void>} A promise that resolves when the fetch process completes.
  *
